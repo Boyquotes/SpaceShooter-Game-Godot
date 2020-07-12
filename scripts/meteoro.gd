8b90cc39adf05a5aef0fd14553aa56f82b80e115
TamiresV <tamiresvilaca@hotmail.com>
2020-07-11 21:04:21 -0300

    Collisions

--- scripts/meteoro.gd
+++ scripts/meteoro.gd
@@ -1,9 +1,11 @@
-extends Node2D
+extends Area2D
 
 var vel = 250
 var rot = 0
+var vida = 4
 
 func _ready():
+	add_to_group(game.GRUPO_INIMIGO)
 	randomize()#cria semente aleatória
 	rot = rand_range(-2,2)#cria angulo de rotação aleatório
 	set_process(true)
@@ -12,4 +14,10 @@ func _ready():
 func _process(delta):
 	set_pos(get_pos() + Vector2(0,1) * vel * delta)
 	rotate(rot * delta)
+	pass
+	
+func aplica_dano(valor):
+	vida -= valor
+	if vida<= 0:
+		queue_free()
 	pass
--- scripts/tiro.gd
+++ scripts/tiro.gd
@@ -1,4 +1,4 @@
-extends Node2D
+extends Area2D
 
 var vel = 500
 
@@ -16,3 +16,16 @@ func _process(delta):
 		queue_free()
 		pass
 	pass
+
+func _on_tiro_area_enter( area ):
+	#se for inimigo destroi
+	if area.is_in_group(game.GRUPO_INIMIGO):
+		if area.has_method("aplica_dano"):
+			area.aplica_dano(1)
+		#se objeto tiver só 1 vida ele será destruido
+		else:
+			area.queue_free()
+			
+		queue_free()
+		pass
+	pass # replace with function body
